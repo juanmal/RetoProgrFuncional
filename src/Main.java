b916e334bc7e17819aa6b24f2b3c728c3adf4e09
Juanma L <juanmalubian@gmail.com>
2021-10-22 17:33:07 +0200

    esPrimo implementado funcional

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Main {
 	
 	public class Operaciones {
 		public static boolean esPrimo(int numero)  {
-			for (int i = 2; i < numero; i++)
-	            if (numero % i == 0)
-	                return false;
+			// Genera una lista desde 2 a numero		
 			
+			// Para todo el rango 2 -> numero, aplica un filtro calculando el resto de la division del numero y todos los valores menores mayores que 1,
+			// pasando el filtro unicamente los que sean numeros primos, si el resultado obtenido es igual al numero recibido por parametro, es primo
+			
+			int res = IntStream.rangeClosed(2, numero).filter(x -> numero % x == 0).sum();
+
+			if (res > numero)
+				return false;
+
 			return true;
 		}
 	}
 	
 	public static void main(String[] args) {
 		List<Integer> numeros = Stream.iterate(1, n -> n + 1).limit(10).collect(Collectors.toList());
-		List<String> nombres = new ArrayList<>(Arrays.asList("juan", "pepe", "juanma", "alex"));
+		List<String> nombres = new ArrayList<>(Arrays.asList("juan antonio", "pepe andres", "juan manuel", "alex perez"));
 		
 		numerosPrimosFuncional(numeros);
 		
-		System.out.println(numeros.stream().filter(x -> x%2==0).mapToInt(x -> x).sum());
+//		System.out.println(numeros.stream().filter(x -> x%2==0).mapToInt(x -> x).sum());
+		convierteNombresCamelCase(nombres);
 	}
 
 	/**
@@ -35,5 +44,11 @@ public class Main {
 		List<Integer> primos = numeros.stream().filter(x -> Operaciones.esPrimo(x)).collect(Collectors.toList());
 
 		System.out.println(primos);		
+	}
+	
+	public static void convierteNombresCamelCase(List<String> nombres) {
+//		List<String> nombresCamelCase = nombres.stream().map(x -> x.split(" ")).collect(Collectors.groupingBy(x -> x[1], ));
+//		
+//		System.out.println(nombresCamelCase);
 	}
 }
