04c35a26cf8b3e82fd5f2a41966894e6c117dfd6
Juanma L <juanmalubian@gmail.com>
2021-10-22 14:39:04 +0200

    Commit inicial

--- src/Main.java
+++ src/Main.java
@@ -26,9 +26,12 @@ public class Main {
 		List<String> nombres = new ArrayList<>(Arrays.asList("juan", "pepe", "juanma", "alex"));
 		
 		long t_ini = System.currentTimeMillis();
+		// Hago un recorrido sobre la lista numero mediante el .stream(), le aplico el filtro llamando a una funcion
+		// para comprobar si es numero primo, y almaceno los elementos del stream que superen el filtro en una lista. 
 		List<Integer> primos = numeros.stream().filter(x -> Operaciones.esPrimo(x)).collect(Collectors.toList());
 		long t_fin = System.currentTimeMillis();
 		System.out.println("Tiempo: " + ((t_fin - t_ini)/1000));
+		
 		System.out.println(primos);
 		
 	}
